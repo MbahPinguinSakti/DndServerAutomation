abd4db3570a9005f0970e5a429e9d85f96879752
Pinguin_ubuntu <edwardrafael2348@gmail.com>
2024-09-09 13:27:25 +0700

    fix: error modifer file code

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e  
 # Root Checker
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root. Please use 'sudo' or log in as root."
@@ -53,8 +54,8 @@ echo "cheking bind9 service"
 #cheking bind9.service
 if ! systemctl is-active --quiet named; then
     echo "bind9 tidak aktif, sedang menginstal bind9..."
-    apt update
-    apt upgrade
+    # apt update
+    # apt upgrade
     apt install bind9 -y
     echo "bind9 telah berhasil diinstal."
 else
@@ -83,15 +84,15 @@ sleep 1
 read -p "enter your ip address: " ip_add
 echo "creating configuration"
 octets=(${ip_add//./ })
-
 # Hapus oktet keempat
 octets=("${octets[0]}" "${octets[1]}" "${octets[2]}")
-
 # Balikkan urutan oktet
 reversed_octets=("${octets[2]}" "${octets[1]}" "${octets[0]}")
-
 # Gabungkan oktet yang dibalik dengan titik
-reversed_ip="${reversed_octets[0]}.${reversed_octets[1]}.${reversed_octets[2]}"   echo "$reversed_ip"
+reversed_ip="${reversed_octets[0]}.${reversed_octets[1]}.${reversed_octets[2]}" 
+
+echo "$reversed_ip"
+
 sleep 1
 
 #manip records forwardZ file
@@ -114,78 +115,14 @@ cp /etc/bind/db.127 ${reverse}_temp &&
 
 echo "${reverse} zone has been sucess fully configured"
 
-# cp /etc/bind/named.conf.default-zones /etc/bind/named.conf.default-zones_temp &&
-#     sed -i "s/localhost/${domain}/g" /etc/bind/named.conf.default-zones_temp && \
-#     sed -i "s/local/ db.${domain}/g" /etc/bind/named.conf.default-zones_temp && \
-#     sed -i "s/127/${reversed_ip}/g" /etc/bind/named.conf.default-zones_temp && \
-#     sed -i "s/db.127/db.${reverse}/g" /etc/bind/named.conf.default-zones_temp && \
-#     mv /etc/bind/named.conf.default-zones_temp /etc/bind/named.conf.default-zones
+named="/etc/bind/named.conf.default-zones"
 
-#!/bin/bash
+cp ${named} ${named}_temp && \
+    sed -i "s/localhost/${domain}/g" "${named}_temp" && \
+    sed -i "s/127/${reversed_ip}/g" "${named}_temp" && \
+    sed -i "s/db.local/db.${domain}/g" "${named}_temp" && \
+    sed -i "s/db.127/db.${first_octet}" "${named}_temp" && \
+    mv ${named}_temp ${named}
 
-# Cek apakah variabel domain dan reversed_ip sudah didefinisikan
-if [ -z "$domain" ]; then
-    echo "Variabel domain tidak didefinisikan"
-    exit 1
-fi
-
-if [ -z "$reversed_ip" ]; then
-    echo "Variabel reversed_ip tidak didefinisikan"
-    exit 1
-fi
-
-# Cek apakah file /etc/bind/named.conf.default-zones ada dan dapat diakses
-if [ ! -f "/etc/bind/named.conf.default-zones" ]; then
-    echo "File /etc/bind/named.conf.default-zones tidak ada"
-    exit 1
-fi
-
-if [ ! -r "/etc/bind/named.conf.default-zones" ]; then
-    echo "File /etc/bind/named.conf.default-zones tidak dapat diakses"
-    exit 1
-fi
-
-# Cek apakah Anda memiliki hak akses yang cukup untuk menulis file /etc/bind/named.conf.default-zones
-if [ ! -w "/etc/bind/named.conf.default-zones" ]; then
-    echo "Anda tidak memiliki hak akses yang cukup untuk menulis file /etc/bind/named.conf.default-zones"
-    exit 1
-fi
-
-# Jalankan perintah cp dan sed
-cp /etc/bind/named.conf.default-zones /etc/bind/named.conf.default-zones_temp
-if [ $? -ne 0 ]; then
-    echo "Gagal melakukan perintah cp"
-    exit 1
-fi
-
-sed -i "s/localhost/${domain}/g" /etc/bind/named.conf.default-zones_temp
-if [ $? -ne 0 ]; then
-    echo "Gagal melakukan perintah sed 1"
-    exit 1
-fi
-
-sed -i "s/local/ db.${domain}/g" /etc/bind/named.conf.default-zones_temp
-if [ $? -ne 0 ]; then
-    echo "Gagal melakukan perintah sed 2"
-    exit 1
-fi
-
-sed -i "s/127/${reversed_ip}/g" /etc/bind/named.conf.default-zones_temp
-if [ $? -ne 0 ]; then
-    echo "Gagal melakukan perintah sed 3"
-    exit 1
-fi
-
-sed -i "s/db.127/db.${reverse}/g" /etc/bind/named.conf.default-zones_temp
-if [ $? -ne 0 ]; then
-    echo "Gagal melakukan perintah sed 4"
-    exit 1
-fi
-
-mv /etc/bind/named.conf.default-zones_temp /etc/bind/named.conf.default-zones
-if [ $? -ne 0 ]; then
-    echo "Gagal melakukan perintah mv"
-    exit 1
-fi
-
+# 
 echo "Proses berhasil"
